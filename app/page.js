2d1d0c021d727b4120570bebe23367562946c311
vefa <44091688+veefa@users.noreply.github.com>
2023-05-30 17:56:26 +0800

    scratch

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 import Image from 'next/image'
+import styles from '../styles/globals.css'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div></div>
+    <div>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <section className='bg-purple-400'></section>
+      </main> 
+    </div>
 
-    </main>
 
   )
 }
